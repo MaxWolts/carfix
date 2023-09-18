4f786bdce7b52b442782de165ce46229e0f1c1d4
MaxWolts <max.wolts@gmail.com>
2023-09-18 17:37:42 -0300

    :sparkles: Banner component and Home page added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header/Header'
+import Banner from '../components/Banner/Banner'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Banner/>
+    </div>
+  )
+}
+
+export default Home
