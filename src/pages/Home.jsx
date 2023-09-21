1c6858a65d3e3f186c2e0add1d21a9f254b31be8
MaxWolts <max.wolts@gmail.com>
2023-09-21 05:04:08 -0300

    :sparkles: SummaryAbout added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header/Header'
 import Banner from '../components/Banner/Banner'
 import Carousel from '../components/Carousel/Carousel'
+import SummaryAbout from '../components/SummaryAbout/SummaryAbout'
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <Header/>
         <Banner/>
         <Carousel/>
+        <SummaryAbout/>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+    Unfonts({
+      google: {
+        families: [{
+          name: 'Public Sans',
+          styles: 'wght@300;400;700',
+          defer: true
+        }],
+      },
+    }),
+  ],
 })
