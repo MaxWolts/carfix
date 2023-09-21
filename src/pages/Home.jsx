81a59128bf84294e5adccd27108014192a6d6371
MaxWolts <max.wolts@gmail.com>
2023-09-21 08:56:18 -0300

    :sparkles: Testimonials added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Header from '../components/Header/Header'
 import Banner from '../components/Banner/Banner'
 import Carousel from '../components/Carousel/Carousel'
 import SummaryAbout from '../components/SummaryAbout/SummaryAbout'
+import Testimonials from '../components/Testimonials/Testimonials'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
         <Banner/>
         <Carousel/>
         <SummaryAbout/>
+        <Testimonials/>
     </div>
   )
 }
